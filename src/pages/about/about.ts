5cec98a001c9ed9949d08e74c7c35824066ffc0c
ikismail <ikismail7@gmail.com>
2018-06-21 13:41:37 +0530

    Updated About us Page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: "page-about",
+  templateUrl: "about.html"
 })
 export class AboutPage {
+  conferenceDate = "2018-06-27";
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor() {}
 }
